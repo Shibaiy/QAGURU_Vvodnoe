aa1dbbd62db608f1657e6cc0bdc662896f7c0c25
maxim.shibaev <shibaiy1991@yandex.ru>
2024-10-01 23:55:29 +0300

    Add test yahoo

--- Google_search.py
+++ Google_search.py
@@ -3,7 +3,7 @@ from selene import browser, be, have
 def test_google_search():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+    browser.element('[id="search"]=))))').should(have.text('User-oriented Web UI browser tests in Python'))
     browser.quit()
 
 def test_yahoo_search():
